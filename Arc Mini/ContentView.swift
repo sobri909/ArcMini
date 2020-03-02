494d6aa7e76b8be4b89d7ad59fcde6b0bbd1aeac
Matt Greenfield <matt@bigpaua.com>
2020-03-02 15:18:40 +0700

    Initial Commit

---
+++ Arc Mini/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Arc Mini
+//
+//  Created by Matt Greenfield on 2/3/20.
+//  Copyright © 2020 Matt Greenfield. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
