97c393bbbe8934f4fc8f02c27b48e835dba45e8f
Matt Greenfield <matt@bigpaua.com>
2020-04-21 21:43:04 +0700

    styling cleanups to segments view #33

--- Arc Mini/Views/TimelineItem/ItemSegmentsView.swift
+++ Arc Mini/Views/TimelineItem/ItemSegmentsView.swift
@@ -24,32 +24,78 @@ struct ItemSegmentsView: View {
     }()
     
     var body: some View {
-        List {
-            ForEach(timelineItem.segmentsByActivityType.reversed(), id: \.id) { segment in
-                ZStack {
-                    HStack {
-                        Circle().fill(Color(segment.activityType?.color ?? UIColor.black)).frame(width: 10, height: 10)
-                        if segment.isDataGap {
-                            Text("Data Gap".localised())
-                                .font(.system(size: 17, weight: .regular))
-                                .foregroundColor(.red)
-
-                        } else {
-                            Text(segment.activityType?.displayName.capitalized.localised() ?? "Unknown".localised())
-                                .font(.system(size: 17, weight: .regular))
-                        }
-                        Spacer()
-                        Text(self.rightText(for: segment))
-                            .font(.system(size: 15, weight: .semibold))
-                            .foregroundColor(Color(UIColor.arcGray1))
-                    }
-                    NavigationLink(destination: ItemSegmentEditView(itemSegment: segment, classifierResults: segment.classifierResults!)) {
-                        EmptyView()
-                    }
-                    .buttonStyle(PlainButtonStyle())
+        GeometryReader { metrics in
+            List {
+                VStack(alignment: .leading, spacing: 0) {
+                    Spacer().frame(height: 24)
+                    Text("Segments")
+                        .font(.system(size: 24, weight: .bold))
+                        .padding([.leading, .trailing], 20)
+                        .frame(height: 30)
+                    Spacer().frame(height: 4)
+                    Text("Each timeline item is made up of one or more recorded activities. To make corrections, tap on rows below or dots on the map.")
+                        .font(.system(size: 16, weight: .regular))
+                        .foregroundColor(Color("brandTertiaryLight"))
+                        .padding([.leading, .trailing], 20)
+                    Spacer().frame(height: 24)
+                    Rectangle().fill(Color("brandSecondary10"))
+                        .frame(width: metrics.size.width, height: 8)
+                    Spacer().frame(height: 24)
+                    Text("END")
+                        .font(.system(size: 10, weight: .regular))
+                        .foregroundColor(Color(0xE35641))
+                        .multilineTextAlignment(.center)
+                        .frame(width: metrics.size.width - 40, height: 24)
+                        .overlay(RoundedRectangle(cornerRadius: 9.5).stroke(Color(0xE5634F), lineWidth: 1).opacity(0.2))
+                        .padding([.leading, .trailing], 20)
+                    Spacer().frame(height: 16)
                 }
+                .listRowInsets(EdgeInsets())
+                .background(Color("background"))
+                ForEach(self.timelineItem.segmentsByActivityType.reversed(), id: \.id) { segment in
+                    ZStack {
+                        HStack {
+                            Circle().fill(Color(segment.activityType?.color ?? UIColor.black)).frame(width: 10, height: 10)
+                            if segment.isDataGap {
+                                Text("Data Gap".localised())
+                                    .font(.system(size: 17, weight: .regular))
+                                    .foregroundColor(.red)
+
+                            } else {
+                                Text(segment.activityType?.displayName.capitalized.localised() ?? "Unknown".localised())
+                                    .font(.system(size: 17, weight: .regular))
+                            }
+                            Spacer()
+                            Text(self.rightText(for: segment))
+                                .font(.system(size: 15, weight: .semibold))
+                                .foregroundColor(Color(UIColor.arcGray1))
+                        }
+                        NavigationLink(destination: ItemSegmentEditView(itemSegment: segment, classifierResults: segment.classifierResults!)) {
+                            EmptyView()
+                        }.hidden()
+                    }
+                    .padding([.leading, .trailing], 20)
+                    .frame(height: 44)
+                    .listRowInsets(EdgeInsets())
+                    .background(Color("background"))
+                }
+                VStack(alignment: .leading, spacing: 0) {
+                    Spacer().frame(height: 16)
+                    Text("START")
+                        .font(.system(size: 10, weight: .regular))
+                        .foregroundColor(Color(0x12A656))
+                        .multilineTextAlignment(.center)
+                        .frame(width: metrics.size.width - 40, height: 24)
+                        .overlay(RoundedRectangle(cornerRadius: 9.5).stroke(Color(0x12A656), lineWidth: 1).opacity(0.2))
+                        .padding([.leading, .trailing], 20)
+                    Spacer().frame(height: 24)
+                }
+                .listRowInsets(EdgeInsets())
+                .background(Color("background"))
             }
+            .environment(\.defaultMinListRowHeight, 44)
         }
+        .background(Color("background"))
         .navigationBarHidden(true)
         .navigationBarTitle("", displayMode: .inline)
         .onAppear {
