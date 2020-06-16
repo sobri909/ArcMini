f40203ecb3fc0c1833232552745876bf67e40911
Matt Greenfield <matt@bigpaua.com>
2020-06-16 22:22:23 +0700

    go fully headless in background if not the current recorder

--- Arc Mini/Core/SceneDelegate.swift
+++ Arc Mini/Core/SceneDelegate.swift
@@ -8,37 +8,26 @@
 
 import UIKit
 import SwiftUI
+import LocoKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var scene: UIScene?
 
     var mapState = MapState()
     var timelineState = TimelineState()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            let rootView = RootView()
-                .environmentObject(timelineState)
-                .environmentObject(mapState)
-            window.rootViewController = UIHostingController(rootView: rootView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        self.scene = scene
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
         logger.info("sceneDidDisconnect")
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        timelineState.sceneDidBecomeActive()
+        growAFullHead()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {}
@@ -46,7 +35,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {}
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        timelineState.sceneDidEnterBackground()
+        if LocomotionManager.highlander.recordingState == .standby {
+            goFullyHeadless()
+        }
+    }
+
+    // MARK: -
+
+    func growAFullHead() {
+        onMain {
+            guard self.window == nil else { return }
+            guard let scene = self.scene as? UIWindowScene else { return }
+            let window = UIWindow(windowScene: scene)
+            let rootView = RootView()
+                .environmentObject(self.timelineState)
+                .environmentObject(self.mapState)
+            window.rootViewController = UIHostingController(rootView: rootView)
+            self.window = window
+            window.makeKeyAndVisible()
+            logger.info("GREW A FULL HEAD")
+        }
+    }
+
+    func goFullyHeadless() {
+        onMain {
+            guard self.window != nil else { return }
+            self.window?.rootViewController?.view.removeFromSuperview()
+            self.window?.rootViewController = nil
+            self.window = nil
+            self.mapState.flush()
+            logger.info("WENT FULLY HEADLESS")
+        }
     }
 
 }
