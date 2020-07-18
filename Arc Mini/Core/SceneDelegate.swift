eb3f0201f33259ac6b603ca9954724da73dd88b3
Matt Greenfield <matt@bigpaua.com>
2020-07-18 21:53:12 +0700

    headless / grow a head tweaks for efficiency / correctness

--- Arc Mini/Core/SceneDelegate.swift
+++ Arc Mini/Core/SceneDelegate.swift
@@ -45,7 +45,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        growAFullHead()
         Jobs.highlander.didBecomeActive()
 
         // take over recording in foreground
@@ -53,12 +52,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let appGroup = loco.appGroup, !appGroup.isAnActiveRecorder {
             loco.becomeTheActiveRecorder()
         }
+
+        growAFullHead()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        if LocomotionManager.highlander.recordingState == .standby {
-            goFullyHeadless()
-        }
         Jobs.highlander.didEnterBackground()
     }
 
@@ -68,6 +66,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         onMain {
             guard self.window == nil else { return }
             guard let scene = self.scene as? UIWindowScene else { return }
+
             let window = UIWindow(windowScene: scene)
             let rootView = RootView()
                 .environmentObject(self.timelineState)
@@ -75,6 +74,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window.rootViewController = UIHostingController(rootView: rootView)
             self.window = window
             window.makeKeyAndVisible()
+
             logger.info("GREW A FULL HEAD")
         }
     }
@@ -82,10 +82,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func goFullyHeadless() {
         onMain {
             guard self.window != nil else { return }
+            guard LocomotionManager.highlander.applicationState != .active else { return }
+
             self.window?.rootViewController?.view.removeFromSuperview()
             self.window?.rootViewController = nil
             self.window = nil
             self.mapState.flush()
+
             logger.info("WENT FULLY HEADLESS")
         }
     }
