f1e435c934fc940aeb30295bdb0dea0eac66d532
Matt Greenfield <matt@bigpaua.com>
2020-11-15 21:32:51 +0700

    more comprehensive system debug view

--- Arc Mini/Views/Debug/SystemDebugView.swift
+++ Arc Mini/Views/Debug/SystemDebugView.swift
@@ -15,24 +15,29 @@ struct SystemDebugView: View {
         NavigationView {
             List {
                 Section(header: Text("General")) {
-                    self.row(leftText: "Thermal state", rightText: AppDelegate.thermalState.stringValue)
-                    self.row(leftText: "Memory footprint", rightText: AppDelegate.memoryString ?? "?")
+                    self.row(leftText: "Thermal state", right: Text(AppDelegate.thermalState.stringValue))
+                    self.row(leftText: "Memory footprint", right: Text(AppDelegate.memoryString ?? "?"))
                 }
                 Section(header: Text("Task Queues")) {
-                    self.row(leftText: "Primary queue jobs", rightText: String(describing: Jobs.highlander.primaryQueue.operationCount))
-                    self.row(leftText: "Secondary queue jobs", rightText: String(describing: Jobs.highlander.secondaryQueue.operationCount))
+                    self.row(leftText: "Primary queue jobs", right: Text("\(Jobs.highlander.primaryQueue.operationCount)"))
+                    self.row(leftText: "Secondary queue jobs", right: Text("\(Jobs.highlander.secondaryQueue.operationCount)"))
                     NavigationLink(destination: PlacesPendingUpdateView()) {
-                        self.row(leftText: "Places pending update",
-                                 rightText: String(describing: RecordingManager.store.placesPendingUpdate))
+                        self.row(leftText: "Places pending update", right: Text("\(RecordingManager.store.placesPendingUpdate)"))
                     }
                     NavigationLink(destination: ModelsPendingUpdateView()) {
-                        self.row(leftText: "UD models pending update",
-                                 rightText: String(describing: RecordingManager.store.modelsPendingUpdate))
+                        self.row(leftText: "UD models pending update", right: Text("\(RecordingManager.store.modelsPendingUpdate)"))
                     }
+                }
+                Section(header: Text("Pending Backups")) {
+                    self.row(leftText: "Notes pending backup", right: Text("\(Backups.backupNotesCount)"))
+                    self.row(leftText: "Places pending backup", right: Text("\(Backups.backupPlacesCount)"))
+                    self.row(leftText: "Timeline Summaries pending backup", right: Text("\(Backups.backupTimelineSummariesCount)"))
+                    self.row(leftText: "Items pending backup", right: Text("\(Backups.backupItemsCount)"))
+                    self.row(leftText: "Samples pending backup", right: Text("\(Backups.backupSamplesCount)"))
                 }
                 self.taskRows
             }
-            .navigationBarTitle("Arc Mini \(Bundle.versionNumber) (\(Bundle.buildNumber))")
+            .navigationBarTitle("Arc Mini \(Bundle.versionNumber) (\(String(format: "%d", Bundle.buildNumber)))")
             .environment(\.defaultMinListRowHeight, 28)
         }
     }
@@ -41,27 +46,46 @@ struct SystemDebugView: View {
 
     var taskRows: some View {
         Section(header: Text("Task States")) {
-            ForEach(TasksManager.highlander.taskStates.sorted { $0.key.rawValue < $1.key.rawValue }, id: \.0) { identifier, status in
-                self.row(leftText: String(identifier.rawValue.split(separator: ".").last!),
-                         rightText: "\(status.state.rawValue) (\(String(duration: -status.lastUpdated.timeIntervalSinceNow, style: .short, maximumUnits: 1)) ago)")
+            ForEach(TasksManager.highlander.taskStates.sorted { $0.value.lastUpdated > $1.value.lastUpdated }, id: \.0) { identifier, status in
+                self.row(leftText: taskNameString(for: status, identifier: identifier), right: Text(statusString(for: status)))
             }
         }
     }
 
     // MARK: -
 
-    func row(leftText: String, rightText: String) -> some View {
+    func row(leftText: String, right rightText: Text) -> some View {
         return HStack {
             Text(leftText).font(.system(.footnote))
             Spacer()
-            Text(rightText).font(.system(.footnote)).opacity(0.6)
+            rightText.font(.system(.footnote)).opacity(0.6)
         }
     }
-
-}
-
-struct SystemDebugView_Previews: PreviewProvider {
-    static var previews: some View {
-        SystemDebugView()
+    
+    func taskNameString(for task: TasksManager.TaskStatus, identifier: TasksManager.TaskIdentifier) -> String {
+        let shortName = identifier.rawValue.split(separator: ".").last!
+        if task.state == .running {
+            return "▶︎ " + shortName
+        }
+        if let lastCompleted = task.lastCompleted, (task.minimumDelay == 0 || lastCompleted.age < task.minimumDelay) {
+            if task.state == .scheduled {
+                return "✓ ▷ " + shortName
+            }
+            return "✓ " + shortName
+        }
+        if task.state == .scheduled {
+            return "▷ " + shortName
+        }
+        return String(shortName)
     }
+    
+    func statusString(for task: TasksManager.TaskStatus) -> String {
+        if task.state == .running { return "running" }
+        if let lastCompleted = task.lastCompleted {
+            return "completed \(String(duration: -lastCompleted.timeIntervalSinceNow, style: .short, maximumUnits: 1)) ago"
+        }
+        if task.state == .registered { return "registered" }
+        return "\(task.state.rawValue) \(String(duration: -task.lastUpdated.timeIntervalSinceNow, style: .short, maximumUnits: 1)) ago"
+    }
+
 }
