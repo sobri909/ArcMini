2b6e0d41794d839728ca9c0952c23c58a0f790b4
Matt <matt@bigpaua.com>
2022-11-23 14:43:13 +0700

    remove system debug rows not relevant to Mini, and group tasks by state

--- Arc Mini/Views/Debug/SystemDebugView.swift
+++ Arc Mini/Views/Debug/SystemDebugView.swift
@@ -28,11 +28,6 @@ struct SystemDebugView: View {
                     self.row(leftText: "Memory footprint", right: Text(AppDelegate.memoryString ?? "?"))
                 }
                 Section(header: Text("Task Queues")) {
-                    self.row(leftText: "Primary queue jobs", right: Text("\(Jobs.highlander.primaryQueue.operationCount)"))
-                    self.row(leftText: "Secondary queue jobs", right: Text("\(Jobs.highlander.secondaryQueue.operationCount)"))
-                    if Backups.backupQueue.operationCount > 0 {
-                        self.row(leftText: "Backup queue jobs", right: Text("\(Backups.backupQueue.operationCount)"))
-                    }
                     if Backups.samplesBackupQueue.operationCount > 0 {
                         self.row(leftText: "Backup samples queue jobs", right: Text("\(Backups.samplesBackupQueue.operationCount)"))
                     }
@@ -50,19 +45,22 @@ struct SystemDebugView: View {
                     .listRowInsets(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
                 }
                 backupRows
-                taskRows
-#if DEBUG || MERELEASE
-                debugActionRows
-#endif
+                if !runningTasks.isEmpty {
+                    runningTaskRows
+                }
+                if !waitingTasks.isEmpty {
+                    waitingTaskRows
+                }
+                otherTaskRows
             }
             .navigationBarTitle("Arc \(Bundle.versionNumber) (\(String(format: "%d", Bundle.buildNumber)))")
             .environment(\.defaultMinListRowHeight, 28)
             .sheet(item: $selectedTask) { item in
                 if #available(iOS 16.0, *) {
-                    taskDetails(for: item)
-                        .presentationDetents([.height(240)])
+                    taskDetailsSheet(for: item)
+                        .presentationDetents([.height(280)])
                 } else {
-                    taskDetails(for: item)
+                    taskDetailsSheet(for: item)
                 }
             }
         }
@@ -88,45 +86,64 @@ struct SystemDebugView: View {
         }
     }
 
-    var taskRows: some View {
-        Section(header: Text("Task States")) {
-            let sortedStates = TasksManager.highlander.taskStates.sorted {
-                if $0.value.state == $1.value.state {
-                    return $0.value.lastCompleted ?? $0.value.lastUpdated - .oneYear > $1.value.lastCompleted ?? $0.value.lastUpdated - .oneYear
-                } else {
-                    return $0.value.state.sortIndex < $1.value.state.sortIndex
-                }
-            }.filter { !TasksManager.TaskIdentifier.deprecatedIdentifiers.contains($0.key) }
-
-            ForEach(sortedStates, id: \.0) { identifier, status in
-                row(leftText: taskNameString(for: status, identifier: identifier), right: Text(statusString(for: status)), showChevron: true)
-                    .onTapGesture { selectedTask = status }
+    var runningTaskRows: some View {
+        Section(header: Text("Running Tasks")) {
+            ForEach(runningTasks, id: \.shortName) { status in
+                row(
+                    leftText: taskNameString(for: status),
+                    right: Text(statusString(for: status)),
+                    showChevron: true
+                )
+                .onTapGesture { selectedTask = status }
             }
         }
     }
 
-    var debugActionRows: some View {
-        Section(header: Text("Debug Actions")) {
-            if copyingDatabase {
-                Text("Copying database...")
-                    .font(.system(size: 13, weight: .regular))
-            } else {
-                Button {
-                    Task {
-                        copyingDatabase = true
-                        RecordingManager.store.copyDatabasesToLocal()
-                        copyingDatabase = false
-                    }
-                } label: {
-                    Text("Copy LocoKit database to app container")
-                        .font(.system(size: 13, weight: .regular))
-                        .foregroundColor(Color(uiColor: .link))
-                }
+    var waitingTaskRows: some View {
+        Section(header: Text("Scheduled Tasks")) {
+            ForEach(waitingTasks, id: \.shortName) { status in
+                row(
+                    leftText: taskNameString(for: status),
+                    right: Text(statusString(for: status)),
+                    showChevron: true
+                )
+                .onTapGesture { selectedTask = status }
             }
         }
     }
 
-    func taskDetails(for task: TasksManager.TaskStatus) -> some View {
+    var otherTaskRows: some View {
+        Section(header: Text("Other tasks")) {
+            ForEach(otherTasks, id: \.shortName) { status in
+                row(
+                    leftText: taskNameString(for: status),
+                    right: Text(statusString(for: status)),
+                    showChevron: true
+                )
+                .onTapGesture { selectedTask = status }
+            }
+        }
+    }
+
+    var runningTasks: [TasksManager.TaskStatus] {
+        return TasksManager.highlander.taskStates.values
+            .filter { $0.state == .running }
+    }
+
+    var waitingTasks: [TasksManager.TaskStatus] {
+        return TasksManager.highlander.taskStates.values
+            .filter { $0.state == .scheduled }
+            .sorted { $0.lastCompleted ?? $0.lastUpdated - .oneYear > $1.lastCompleted ?? $1.lastUpdated - .oneYear }
+    }
+
+    var otherTasks: [TasksManager.TaskStatus] {
+        return TasksManager.highlander.taskStates.values
+            .filter { $0.state != .running && $0.state != .scheduled }
+            .filter { !TasksManager.TaskIdentifier.deprecatedIdentifiers.contains($0.shortName) }
+            .sorted { $0.state.sortIndex < $1.state.sortIndex }
+    }
+
+    func taskDetailsSheet(for task: TasksManager.TaskStatus) -> some View {
         VStack(spacing: 0) {
             HStack {
                 Text(task.shortName).font(.headline)
@@ -192,31 +209,37 @@ struct SystemDebugView: View {
         .frame(height: 28)
         .listRowInsets(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
     }
-    
-    func taskNameString(for task: TasksManager.TaskStatus, identifier: String) -> String {
+
+    func taskNameString(for task: TasksManager.TaskStatus) -> String {
         if task.state == .expired {
-            return "✕ " + identifier
+            return "✕ " + task.shortName
         }
         if task.state == .unfinished {
-            return "!! " + identifier
+            return "!! " + task.shortName
         }
         if task.state == .running {
-            return "▶︎ " + identifier
+            return "▶︎ " + task.shortName
         }
         if let lastCompleted = task.lastCompleted, (task.minimumDelay == 0 || lastCompleted.age < task.minimumDelay) {
             if task.state == .scheduled {
-                return "✓ ▷ " + identifier
+                return "✓ ▷ " + task.shortName
             }
-            return "✓ " + identifier
+            return "✓ " + task.shortName
         }
         if task.state == .scheduled {
-            return "▷ " + identifier
+            return "▷ " + task.shortName
         }
-        return String(identifier)
+        return String(task.shortName)
     }
-    
+
     func statusString(for task: TasksManager.TaskStatus) -> String {
-        if task.state == .running { return "running" }
+        if task.state == .running {
+            if let lastStarted = task.lastStarted {
+                return "running for \(String(duration: lastStarted.age, style: .short, maximumUnits: 1))"
+            } else {
+                return "running"
+            }
+        }
         if let lastCompleted = task.lastCompleted {
             return "completed \(String(duration: -lastCompleted.timeIntervalSinceNow, style: .short, maximumUnits: 1)) ago"
         }
